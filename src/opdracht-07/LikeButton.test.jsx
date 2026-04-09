9e61edda4f04330b0fa99535ff2b45aef48fc55f
ArdaUtu <9024283@student.zadkine.nl>
2026-04-09 10:17:17 +0200

    Opdracht 7 klaar

--- src/opdracht-07/LikeButton.test.jsx
+++ src/opdracht-07/LikeButton.test.jsx
@@ -1,35 +1,51 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import LikeButton from './LikeButton';
-
+ 
 describe('LikeButton', () => {
-
+ 
   // VOORBEELD - Deze test is al ingevuld
   test('VOORBEELD: toont initiële aantal likes', () => {
     render(<LikeButton initialLikes={5} />);
     expect(screen.getByText('5 likes')).toBeInTheDocument();
   });
-
+ 
   test('verhoogt likes bij klik', () => {
     // TODO: Render LikeButton met initialLikes={0}
     // TODO: Vind de button (Hint: screen.getByRole('button'))
     // TODO: Klik op de button (Hint: fireEvent.click(button))
     // TODO: Check of "1 likes" op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    render(<LikeButton initialLikes={0} />);
+ 
+    const button = screen.getByRole('button', { name: /Like/i });
+    fireEvent.click(button);
+ 
+    expect(screen.getByText('1 likes')).toBeInTheDocument();
   });
-
+ 
   test('verlaagt likes bij tweede klik', () => {
     // TODO: Render LikeButton
     // TODO: Klik 2x op de button
     // TODO: Check of we terug zijn bij "0 likes"
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    render(<LikeButton initialLikes={0} />);
+ 
+    const button = screen.getByRole('button', { name: /Like/i });
+    fireEvent.click(button);
+    fireEvent.click(button);
+ 
+    expect(screen.getByText('0 likes')).toBeInTheDocument();
   });
-
+ 
   test('toont rood hartje als geliked', () => {
     // TODO: Render LikeButton
     // TODO: Klik op de button
     // TODO: Check of de button tekst "❤️ Like" bevat
     // Hint: screen.getByText(/❤️ Like/)
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    render(<LikeButton initialLikes={0} />);
+ 
+    const button = screen.getByRole('button', { name: /Like/i });
+    fireEvent.click(button);
+ 
+    expect(screen.getByRole('button', { name: /❤️ Like/ })).toBeInTheDocument();
   });
-
+ 
 });
